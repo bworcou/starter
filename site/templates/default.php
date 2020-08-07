72e0dd95cd509aa1325f9321c12ca9e1c72176e6
Bez Worcou <beazaworcou@gmail.com>
2020-08-07 15:09:18 +1000

    test commit week6

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,5 +1,17 @@
-<? snippet('header') ?>
+<?php snippet('header') ?>
 
-    <h1><?= $page->title() ?></h1>
+    <h1><?php echo $page->title() ?></h1>
+<ul>
+  <? $works = page('works') ?>
+  <?= $works->title() ?>
+  <? foreach(page('work')->children() as $single): ?>
+  <li><a href="<?= $single->url() ?>"><?= $single->title() ?>, <?
+    <? foreach(page('contributors')->children() as $contributor): ?>
+    <li><a href="<?= $contributor->url() ?>"><?= $contributor->title() ?></a></li>
+    <? endforeach ?>
+</ul>
 
-<? snippet('footer') ?>
+<? $about = page('about') ?>
+<?= $about->title() ?>
+<?= $about->text()->kirbytext() ?>
+<?php snippet('footer') ?>
